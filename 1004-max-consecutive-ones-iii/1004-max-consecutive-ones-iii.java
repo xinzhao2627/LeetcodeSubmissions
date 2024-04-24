0ee73513176115866a517d48258c4d04a2752f9c
xinzhao2627 <94624435+xinzhao2627@users.noreply.github.com>
2024-04-24 17:38:02 +0800

    Time: 26 ms (6.7%), Space: 46.1 MB (97.91%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int largest = 0, x = 0, y =0, p = k;
+        ArrayList<Integer> z= new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] == 1){
+                x++;
+                y++;
+            } else if (p > 0){ // if 0 and still has pots
+                z.add(++y);
+                x++;
+                y=0;
+                p--;
+            } else if (k != 0){ // if no pots left and nums[i] is 0
+                largest = (x > largest) ? x : largest;
+                int val = 0;
+                val = z.get(0);
+                z.remove(0);
+                x += 1 - val ;
+                z.add(++y);
+                y = 0;
+            } else{
+                largest = (x > largest) ? x : largest;
+                x = 0;
+            }
+        }
+
+        return (x>largest) ? x : largest;
+    }
+}
