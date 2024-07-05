9173253ab8dd2b52b00845ed684db96305e7d3d9
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-07-05 20:42:41 +0800

    Time: 178 ms (58.83%), Space: 12.9 MB (43.67%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+
+
+        
