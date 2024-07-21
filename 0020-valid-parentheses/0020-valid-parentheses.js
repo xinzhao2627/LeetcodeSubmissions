92cf647c4017d3ff14c86c49523441b04c3f91fa
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-07-21 12:49:17 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,27 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if (s.length % 2 !== 0 || s.length === 0 ) return false
-    const unclosed = []
-    const closed_type = [']',  '}', ')']
-    const open_type = ['[', '{', '(']
-    for (let i = 0; i < s.length; i++){
-        const c = s.charAt(i)
-        const index = open_type.indexOf(c)
-        if (index !== -1){
-            unclosed.push(closed_type[index])
-        } else if (unclosed.length !== 0){
-            if (c === unclosed[unclosed.length - 1]) {
-                unclosed.pop()
-            } else {
-                return false
-            }
+    if (s.length % 2 !== 0) return false
+    const par_o = new Map()
+    const par_c = new Map()
+    par_o.set('(', 0)
+    par_c.set(')', 0)
+    par_o.set('[', 0)
+    par_c.set(']', 0)
+    par_o.set('{', 0)
+    par_c.set('}', 0)
+    const close = [')', ']', '}']
+    const open = ['(', '[', '{']
+    for (let i = 0 ; i < s.length; i++){
+        if (close.includes(s[i])){
+            const o = open[close.indexOf(s.charAt(i))]
+            if (par_o.get(o) === 0) return false
+            else par_c.set(s.charAt(i), par_c.get(s.charAt(i)) + 1)
         } else {
-            return false
+            par_o.set(s.charAt(i), par_o.get(s.charAt(i)) +1)
         }
     }
-    if (unclosed.length === 0) return true
-    else {
-        return false
+    for (let [v, k] of par_o){
+        if (v > 0) return false
     }
+    for (let [v, k] of par_c){
+        if (v > 0) return false
+    }
+    return true
+
+
+    
 };
