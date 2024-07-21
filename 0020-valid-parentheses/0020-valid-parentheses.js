4827fb621336eda7e73d55fc2211390c43fe9ee4
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-07-21 13:01:09 +0800

    Time: 57 ms (62.53%), Space: 49.8 MB (75.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,32 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if (s.length % 2 !== 0) return false
-    const par_o = new Map()
-    const par_c = new Map()
-    par_o.set('(', 0)
-    par_c.set(')', 0)
-    par_o.set('[', 0)
-    par_c.set(']', 0)
-    par_o.set('{', 0)
-    par_c.set('}', 0)
+    if (s.length % 2 !== 0 || s.length === 0) return false
+    const toClose = []
     const close = [')', ']', '}']
     const open = ['(', '[', '{']
     for (let i = 0 ; i < s.length; i++){
-        if (close.includes(s[i])){
-            const o = open[close.indexOf(s.charAt(i))]
-            if (par_o.get(o) === 0) return false
-            else par_c.set(s.charAt(i), par_c.get(s.charAt(i)) + 1)
-        } else {
-            par_o.set(s.charAt(i), par_o.get(s.charAt(i)) +1)
-        }
+        const c = s.charAt(i)
+        const index = close.indexOf(c)
+        if (index !== -1){
+            if (toClose.length === 0) return false
+            if (toClose[toClose.length - 1] !== open[index]) return false
+            toClose.pop() 
+        } else toClose.push(c)
     }
-    for (let [v, k] of par_o){
-        if (v > 0) return false
-    }
-    for (let [v, k] of par_c){
-        if (v > 0) return false
-    }
+    if (toClose.length) return false
     return true
 
 
