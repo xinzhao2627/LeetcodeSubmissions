94f726085c1e42cd03207b103765e775abd1258c
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-07-27 12:45:58 +0800

    Time: 48 ms (98.23%), Space: 52.3 MB (62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,19 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let regex = /^[a-zA-Z0-9]+$/
-    let i = 0
-    let j = s.length
-    while (i < j && j > i){
-        if (!regex.test(s.charAt(i))){
-            i++
-            continue
-        } else if (!regex.test(s.charAt(j))){
-            j--
-            continue
-        } else if (s.charAt(i).toUpperCase() !== s.charAt(j).toUpperCase()){
-            return false
-        } else {
+    let fs = s.split(/[^a-zA-Z0-9]+/).join('').toLowerCase()
+    let i = 0, j = fs.length -1
+    while (i<j && j>i){
+        if (fs[i] !== fs[j]) {return false}
+        else{
             i++
             j--
         }
