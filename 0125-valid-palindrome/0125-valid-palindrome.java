24a096eaa18ec28af46a08a557bdc969ee623e5d
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-11-07 19:57:09 +0800

    Time: 3 ms (72.7%), Space: 43.3 MB (53.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,18 @@
 import java.util.*;
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.length() == 0) return false;
-        s = s.replaceAll("[^A-Za-z0-9]+", "").toLowerCase();
+        s = s.toLowerCase();
         int i = 0, j = s.length() - 1;
-        while (i < j && j > i){
-            if (s.charAt(i) == s.charAt(j)){
+        while (i < j){
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            if (!Character.isLetterOrDigit(c1)) i++;
+            else if (!Character.isLetterOrDigit(c2)) j--;
+            else {
+                if (c1 != c2) return false;
                 i++;
                 j--;
-                continue;
-            } else return false;
+            }
         }
         return true;
     }
