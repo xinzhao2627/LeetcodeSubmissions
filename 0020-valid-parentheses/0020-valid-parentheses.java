1a041fad785d1c6916f8b9bb6031817dec7e9b7d
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-11-12 12:00:17 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,22 @@
 import java.util.*;
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 == 1) return false;
+        if (s.length() <= 1) return false;
+
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()){
-            if (c == '(') stack.push(')');
-            else if (c == '[') stack.push(']');
-            else if (c == '{') stack.push('}');
-            else if (stack.isEmpty() || c != stack.pop() ) return false;
+
+        for (char c: s.toCharArray()){
+            if (c == '['){
+                stack.push(']');
+            }else if (c == '{'){
+                stack.push('}');
+            } else if (c == '('){
+                stack.push(')');
+            } else if (!stack.isEmpty() && stack.pop() != c){
+                return false;
+            } 
         }
-        // if there are still parentheses, false.. completed if true
+
         return stack.isEmpty();
     }
 }
