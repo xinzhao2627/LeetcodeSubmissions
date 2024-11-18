41c40e6eaf228cac7bc124570abfdb82a64c9d18
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-11-19 07:17:46 +0800

    Time: 2 ms (41.47%), Space: 45.2 MB (9.87%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,23 +1,22 @@
 import java.util.*;
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        int res_last_val = 0;
-        for (int i = 0; i< intervals.length; i++){
-            if (intervals[i][0] > newInterval[1]){
-                res.add(newInterval);
-                res.add(intervals[i]);
-                for (int j = i+1; j < intervals.length; j++) res.add(intervals[j]);
-                return res.toArray(new int[res.size()][]);
-            } else if (newInterval[0] > intervals[i][1]){
-                res.add(intervals[i]);
+        ArrayList<int[]> res = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++){
+            if (newInterval[1] < intervals[i][0] ){
+                res.add(new int[] {newInterval[0], newInterval[1]});
+                for (int j = i; j< intervals.length; j++) res.add(new int[] {intervals[j][0], intervals[j][1]});
+                return res.toArray(new int[ res.size()][]);
+            } else if (intervals[i][1] < newInterval[0]){
+                res.add(new int [] {intervals[i][0], intervals[i][1]});
             } else {
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             }
         }
 
-        res.add(newInterval);
+        res.add(new int[] {newInterval[0], newInterval[1]});
         return res.toArray(new int[res.size()][]);
     }
 }
