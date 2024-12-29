5d3de25054ccc42d421eb09ffbd8e19dd85ab38f
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-12-29 13:07:11 +0800

    Time: 1 ms (98.24%), Space: 50 MB (64.54%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new List<int[]>();
+
+        for (int i = 0 ; i < intervals.Length; i++){
+            if (intervals[i][0] > newInterval[1]) {
+                res.Add(newInterval);
+                for (int j = i; j< intervals.Length; j++) res.Add(intervals[j]);
+                return res.ToArray();
+            }
+            else if (intervals[i][1] < newInterval[0]){
+                res.Add(intervals[i]);
+            } else {
+                newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
+                newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
+            }
+        }
+        res.Add(newInterval);
+        return res.ToArray();
+    }
+}
