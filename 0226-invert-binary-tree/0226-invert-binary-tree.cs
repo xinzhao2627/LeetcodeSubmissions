1e0e44b656d26da563dcc6772130131f48cf80d3
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-12-29 13:15:58 +0800

    Time: 0 ms (100%), Space: 41.3 MB (5.78%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        InvertTree(root.right);
+        InvertTree(root.left);
+        return root; 
+    }
+}
