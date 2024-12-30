8c80cf9bbbfa271767a4909381e86d162daa4f05
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2024-12-30 17:22:48 +0800

    Time: 4 ms (51.57%), Space: 52.7 MB (9.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,39 @@
 class Solution {
-    public void bfs(int r, int c, char[][]grid, int[][] directions, int r_len, int c_len){
+    public void bfs(int r, int c, int r_len, int c_len, char[][] grid){
+        int[][] directions = new int[][] {{0,-1}, {0,1}, {1,0}, {-1,0}};
         Queue<int[]> q = new LinkedList<>();
         grid[r][c] = '0';
         q.add(new int[] {r, c});
         while (!q.isEmpty()){
             int[] point = q.poll();
+            for (int[] dir : directions){
+                int locr = point[0]+dir[0];
+                int locc = point[1]+dir[1];
 
-            for (int[] direction : directions){
-                int nr = point[0] + direction[0], nc = point[1]+direction[1];
-
-                if (nr < r_len && nr >=0 && 
-                    nc < c_len && nc >= 0 && 
-                    grid[nr][nc] == '1'){
-                    
-                        q.add(new int[] {nr, nc});
-                        grid[nr][nc] = '0';
-                }
-            } 
-        }
-    }
-
-    public int numIslands(char[][] grid) {
-        if (grid.length == 0) return 0;
-        int islands = 0;
-        int r_len = grid.length;
-        int c_len = grid[0].length;
-
-        // direction: {up, down, left, right}
-        int[][] directions = {{1,0}, {-1, 0}, {0, -1}, {0, 1}};
-        for (int i = 0; i < r_len; i++){
-            for (int j = 0; j < c_len; j++){
-                if (grid[i][j] != '0'){
-                    bfs(i, j, grid, directions, r_len, c_len);
-                    islands++;
+                if (locc < c_len && locr < r_len && locc > -1 && locr > -1
+                    && grid[locr][locc] == '1'
+                ){
+                    q.add(new int[] {locr, locc});
+                    grid[locr][locc] = '0';
                 }
             }
         }
 
-        return islands;
     }
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+        int ni = 0;
+        int r_len = grid.length;
+        int c_len = grid[0].length;
+        for (int r = 0; r < r_len; r++){
+            for (int c = 0; c < c_len; c++){
+                if (grid[r][c] != '0'){
+                    bfs(r, c, r_len, c_len, grid);
+                    ni++;
+                }
+            }
+        }
+        return ni;
+    }
+
 }
