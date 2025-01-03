3f2e35e1d4bd3a06db921bb2de1c44beff090dd9
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-03 17:57:36 +0800

    Time: 138 ms (40.76%), Space: 55.7 MB (16.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,39 +1,42 @@
 public class Solution {
-    int[][] directions = new int [][]{
-        new int[] {1,0},
-        new int[] {0,1},
-        new int[] {-1,0},
-        new int[] {0,-1}
+    int[][] directions = new int[][] {
+    new int[] {1,0},
+    new int[] {0,1},
+    new int[] {-1,0},
+    new int[] {0,-1},
     };
-    public void bfs(char[][] grid, int r_len, int c_len, int i , int j){
-        grid[i][j] = '0';
+
+    public void Bfs(int r, int c, int r_len, int c_len, char[][] grid){
         Queue<int[]> q = new Queue<int[]>();
-        q.Enqueue(new int[] {i, j});
-        
-
-
+        q.Enqueue(new int[] {r,c});
+        grid[r][c] = '0';
         while (q.Count() > 0){
-            int[] point = q.Dequeue();
-            foreach(int[] direction in directions){
-                int r  = point[0] + direction[0];
-                int c = point[1] + direction[1];
+            int[] p = q.Dequeue();
 
-                if (r >= 0 && c >= 0 && r < r_len && c < c_len && grid[r][c] != '0'){
-                    q.Enqueue(new int[] {r, c});
-                    grid[r][c] = '0';
+            foreach (int[] d in directions){
+                int targ_r = d[0] + p[0];
+                int targ_c = d[1] + p[1];
+
+                if (targ_r >= 0 && targ_c >= 0 && targ_r < r_len && targ_c < c_len && grid[targ_r][targ_c] == '1'){
+                    q.Enqueue(new int[ ] {targ_r, targ_c});
+                    grid[targ_r][targ_c] = '0';
                 }
-            } 
+
+            }
+
         }
     }
+
+
     public int NumIslands(char[][] grid) {
-        int r_len = grid.Length;
         int c_len = grid[0].Length;
+        int r_len = grid.Length;
         int res = 0;
-        for (int i = 0; i < r_len; i++){
-            for (int j = 0; j < c_len; j++){
-                if (grid[i][j] == '1'){
-                    bfs(grid, r_len, c_len, i, j);
+        for (int r = 0; r < r_len; r++){
+            for (int c = 0; c < c_len; c++){
+                if (grid[r][c] == '1'){
                     res++;
+                    Bfs(r, c, r_len, c_len, grid);
                 }
             }
         }
