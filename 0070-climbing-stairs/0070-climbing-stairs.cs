3d57b411163836928f0ede69f0fe91e9aa55d74e
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-06 20:05:37 +0800

    Time: 0 ms (100%), Space: 27.5 MB (44.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cs
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -2,10 +2,10 @@ public class Solution {
     public int ClimbStairs(int n) {
         if (n <= 3) return n;
         int a = 1, b = 1;
-        for (int i = 0; i < n; i++){
-            int tmp = b;
-            b = a;
-            a+= tmp;
+        for (int i = 0; i < n; i++ ){
+            int tmp = a;
+            a += b;
+            b = tmp;
         }
         return b;
     }
