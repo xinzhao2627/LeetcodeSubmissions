058d82884feea058a2252464826826a1ac811f5a
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-07 15:34:20 +0800

    Time: 1 ms (98.25%), Space: 50.5 MB (35.57%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cs
+++ 0057-insert-interval/0057-insert-interval.cs
@@ -5,18 +5,17 @@ public class Solution {
         for (int i = 0; i < intervals.Length; i++){
             if (intervals[i][0] > newInterval[1]){
                 res.Add(newInterval);
-                for (int j = i; j < intervals.Length; j++) res.Add(intervals[j]);
+                for(int j = i; j < intervals.Length; j++) res.Add(intervals[j]);
                 return res.ToArray();
-            } 
-            else if (intervals[i][1]< newInterval[0]){
+            } else if (newInterval[0] > intervals[i][1]){
                 res.Add(intervals[i]);
-            }
-            else {
-                newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
-                newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
+            } else {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
             }
         }
         res.Add(newInterval);
+
         return res.ToArray();
     }
 }
