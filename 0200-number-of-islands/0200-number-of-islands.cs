977a750d847f9d8a811d66dbbbf1c8f596d7b67f
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-07 15:25:32 +0800

    Time: 142 ms (33.11%), Space: 56.2 MB (15.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,41 +1,38 @@
 public class Solution {
-    int[][] directions = new int[][]{
-        new int[] {1,0},
-        new int[] {0,1},
-        new int[] {-1, 0},
-        new int[] {0, -1}
-    };
-    public void Bfs(int i, int j, char[][] grid, int r_len, int c_len){
-        Queue<int[]> q = new Queue<int[]>();
+    public void bfs(char[][] grid, int r_len, int c_len, int i , int j){
         grid[i][j] = '0';
-        q.Enqueue(new int[]{i,j});
+        Queue<int[]> q = new Queue<int[]>();
+        q.Enqueue(new int[] {i, j});
         
+        int[][] directions = new int [][]{
+            new int[] {1,0},
+            new int[] {0,1},
+            new int[] {-1,0},
+            new int[] {0,-1}
+        };
+
         while (q.Count() > 0){
             int[] point = q.Dequeue();
             foreach(int[] direction in directions){
-                int r = direction[0] + point[0];
-                int c = direction[1] + point[1];
+                int r  = point[0] + direction[0];
+                int c = point[1] + direction[1];
 
-                if (r >= 0 && c >= 0 && r < r_len && c < c_len && grid[r][c] == '1'){
+                if (r >= 0 && c >= 0 && r < r_len && c < c_len && grid[r][c] != '0'){
                     q.Enqueue(new int[] {r, c});
                     grid[r][c] = '0';
                 }
-            }
-
+            } 
         }
-    } 
-
+    }
     public int NumIslands(char[][] grid) {
-
-        int res = 0;
         int r_len = grid.Length;
         int c_len = grid[0].Length;
-
+        int res = 0;
         for (int i = 0; i < r_len; i++){
             for (int j = 0; j < c_len; j++){
                 if (grid[i][j] == '1'){
+                    bfs(grid, r_len, c_len, i, j);
                     res++;
-                    Bfs(i, j, grid, r_len, c_len);
                 }
             }
         }
