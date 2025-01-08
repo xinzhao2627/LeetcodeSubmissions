69672340451e9b1c5036deb49f874f7339ffe9a7
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-08 20:57:05 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int res = 0;
+        int cur = Int32.MaxValue;
+
+        foreach (int p in prices){
+            cur = (p < cur) ? p:cur;
+            res = (p-cur > res) ? p : res;
+        }
+        return res;
+    }
+}
