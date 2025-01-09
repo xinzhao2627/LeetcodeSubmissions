0c2fda2a7c5d1bc33729090dacad9673222354b8
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-09 19:13:14 +0800

    Time: 1 ms (98.13%), Space: 64.2 MB (8.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cs
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -2,10 +2,12 @@ public class Solution {
     public int MaxSubArray(int[] nums) {
         int res = nums[0];
         int cur = nums[0];
+
         for (int i = 1; i < nums.Length; i++){
-            cur = (cur + nums[i] > nums[i]) ? cur + nums[i] : nums[i];
+            cur = (cur + nums[i] > nums[i]) ? cur + nums[i]: nums[i];
             res = (cur > res) ? cur : res;
         }
         return res;
+
     }
 }
