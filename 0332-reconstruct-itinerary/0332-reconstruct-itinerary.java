4d7e161d00c048c06266a88a8010d7e572144de1
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-15 21:07:07 +0800

    Time: 5 ms (99.38%), Space: 45.3 MB (75.57%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,20 +1,21 @@
 class Solution {
-    public void dfs(String airport,List<String> res, Map<String, PriorityQueue<String>> hm){
-        PriorityQueue<String> pq = hm.get(airport);
-        while (pq != null && !pq.isEmpty()){
-            dfs(pq.poll(), res, hm);
+    public void dfs(Map<String , PriorityQueue<String>> airports, String airport, List<String> res){
+        PriorityQueue<String> pq= airports.get(airport);
+        while(pq != null && !pq.isEmpty()){
+            dfs(airports,pq.poll(), res);
         }
         res.addFirst(airport);
+        
     }
-
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> hm = new HashMap<>();
-        List<String> res = new LinkedList<>();
-        for (List<String> t: tickets){
-            hm.putIfAbsent(t.get(0), new PriorityQueue<>());
-            hm.get(t.get(0)).add(t.get(1));
+        Map<String , PriorityQueue<String>> airports = new HashMap<>();
+        List<String> res=  new LinkedList<>();
+        for (List<String> t : tickets){
+            airports.putIfAbsent(t.get(0), new PriorityQueue<>());
+            airports.get(t.get(0)).add(t.get(1));
         }
-        dfs("JFK", res, hm);
+        dfs(airports, "JFK", res);
         return res;
+
     }
 }
