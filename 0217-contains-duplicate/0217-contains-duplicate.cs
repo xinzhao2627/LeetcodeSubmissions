e17094a9928a925c16eb1eb7d987426b304a3c67
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-16 20:02:50 +0800

    Time: 10 ms (86.72%), Space: 69.4 MB (9.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         HashSet<int> ss = new HashSet<int>();
-        foreach (int n in nums) if(!ss.Add(n)) return true;
+        for (int i = 0; i < nums.Length; i++) if(!ss.Add(nums[i])) return true;
         return false;
     }
 }
