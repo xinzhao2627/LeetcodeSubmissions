c130f60281d991fc9e7734d7f330da6953d33658
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-18 15:00:31 +0800

    Time: 2 ms (99.11%), Space: 42.7 MB (96.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length() -1;
+
         while (i < j){
-            char l = Character.toUpperCase(s.charAt(i)), r = Character.toUpperCase(s.charAt(j));
+            char l = s.charAt(i), r = s.charAt(j);
             if (!Character.isLetterOrDigit(l)) {
                 i++;
                 continue;
             } else if (!Character.isLetterOrDigit(r)){
                 j--;
                 continue;
-            } else if (l != r) return false;
+            } else if (Character.toUpperCase(l) != Character.toUpperCase(r)) return false;
             else {
                 i++;
                 j--;
