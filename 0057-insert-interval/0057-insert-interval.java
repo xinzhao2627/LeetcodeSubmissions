c9eb090222d02de09ce2e4cf4cdd833524168007
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-18 11:20:11 +0800

    Time: 2 ms (39.55%), Space: 44.9 MB (55.3%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> res = new ArrayList<>();
+
         for (int i = 0; i < intervals.length; i++){
-            if (intervals[i][0] > newInterval[1]){
+            if (newInterval[1] < intervals[i][0]){
                 res.add(newInterval);
                 for (int j = i; j < intervals.length; j++){
                     res.add(intervals[j]);
@@ -11,14 +12,13 @@ class Solution {
             }
             else if (intervals[i][1] < newInterval[0]){
                 res.add(intervals[i]);
-            }
-            else {
-                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-            }
-        }
-        res.add(newInterval);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
 
+            }
+        } 
+        res.add(newInterval);
         return res.toArray(new int[res.size()][]);
     }
 }
