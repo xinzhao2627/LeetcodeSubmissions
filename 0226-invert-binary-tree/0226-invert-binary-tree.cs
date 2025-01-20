efc342048f37220dd3f2736b1a091a322b05fb04
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-20 20:08:33 +0800

    Time: 0 ms (100%), Space: 41.3 MB (14.09%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cs
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cs
@@ -12,16 +12,17 @@
  * }
  */
 public class Solution {
+    
     public TreeNode InvertTree(TreeNode root) {
         if (root == null) return null;
-        TreeNode tmp = root.left;
-        root.left = root.right;
-        root.right = tmp;
 
+        TreeNode tmp = root.left;
+        root.left=  root.right;
+        root.right = tmp;
         InvertTree(root.left);
         InvertTree(root.right);
 
-
         return root;
+        
     }
 }
