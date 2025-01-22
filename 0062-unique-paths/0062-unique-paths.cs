3e9f7f8c876b965603569c813a5bed9b92c1bc65
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-01-22 19:29:34 +0800

    Time: 0 ms (100%), Space: 28.9 MB (16.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cs
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int UniquePaths(int m, int n) {
-        int[,] grid = new int[m,n];
-
-        for (int r = 0; r < m; r++){
-            for (int c = 0; c < n; c++){
-                grid[r,c] = (r > 0 && c > 0) ? grid[r-1, c] + grid[r, c-1] : 1; 
+        var grid = new int [m,n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                grid[i, j] = (i > 0 && j > 0) ?  grid[i - 1, j] + grid[i, j-1] : 1;
             }
         }
-        return grid[m-1,n-1];
+        return grid[m-1, n-1];
     }
 }
