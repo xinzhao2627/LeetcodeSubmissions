4f5ec76a42dcd531358afff55fbaa21216806c63
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-03-22 21:31:00 +0800

    Time: 28 ms (5.22%), Space: 44.6 MB (44.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,14 @@
-import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        HashMap <Character, Integer> freq1 = new HashMap<>();
-        HashMap <Character, Integer> freq2 = new HashMap<>();
-        for (char c1 : s.toCharArray())
-            freq1.put(c1, ((freq1.containsKey(c1)) ? freq1.get(c1)+1: 1));
-        
-        for (char c2 : t.toCharArray())
-            freq2.put(c2, ((freq2.containsKey(c2)) ? freq2.get(c2)+1: 1));
-        
-        return (freq1.equals(freq2)) ? true: false;
+        HashMap<Character, Integer> res = new HashMap<>();
+        HashMap<Character, Integer> res2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++){
+            res.put(s.charAt(i), res.getOrDefault(s.charAt(i), 0) + 1);
+            res2.put(t.charAt(i), res2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return res.equals(res2);
     }
 }
