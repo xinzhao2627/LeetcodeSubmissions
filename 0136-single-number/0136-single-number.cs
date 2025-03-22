abfb9312684ab510ddd432e433768e69ba7fcd0e
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-03-22 21:52:54 +0800

    Time: 0 ms (100%), Space: 46.6 MB (62.37%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        return nums.Aggregate((res,cur) => res^=cur);        
+        int res = 0;
+        for (int i = 0; i < nums.Length; i++) res ^= nums[i];
+        return res;
     }
 }
