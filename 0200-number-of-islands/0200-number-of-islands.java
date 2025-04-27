1c2e32cf66038ea3d329865a3d7f073e544cbe2c
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-04-27 12:06:24 +0800

    Time: 4 ms (48.6%), Space: 48.7 MB (97.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,43 @@
 class Solution {
-    public void bfs(int r, int c, int r_len, int c_len, char[][] grid){
-        int[][] directions = new int[][] {{0,-1}, {0,1}, {1,0}, {-1,0}};
-        Queue<int[]> q = new LinkedList<>();
-        grid[r][c] = '0';
-        q.add(new int[] {r, c});
-        while (!q.isEmpty()){
-            int[] point = q.poll();
-            for (int[] dir : directions){
-                int locr = point[0]+dir[0];
-                int locc = point[1]+dir[1];
+    int[][] directions = new int[][] {
+        {0, 1},
+        {0, -1},
+        {1, 0},
+        {-1, 0}
+    };
+    public void bfs(char[][] grid, int i, int j, int r, int c){
+        Queue<int[]> queue = new LinkedList<>();
+        grid[i][j] = '0';
 
-                if (locc < c_len && locr < r_len && locc > -1 && locr > -1
-                    && grid[locr][locc] == '1'
-                ){
-                    q.add(new int[] {locr, locc});
-                    grid[locr][locc] = '0';
+        queue.add(new int[]{i, j});
+
+        while (!queue.isEmpty()){
+            int[] el = queue.remove();
+
+            for (int[ ] d : directions){
+                int nr = d[0] + el[0];
+                int nc = d[1] + el[1];
+
+                if (nr >= 0 && nc >= 0 && nr < r && nc < c && grid[nr][nc] == '1'){
+                    grid[nr][nc] = '0';
+                    queue.add(new int[] {nr, nc});
                 }
             }
-        }
+        } 
 
     }
     public int numIslands(char[][] grid) {
-        if (grid.length == 0) return 0;
-        int ni = 0;
-        int r_len = grid.length;
-        int c_len = grid[0].length;
-        for (int r = 0; r < r_len; r++){
-            for (int c = 0; c < c_len; c++){
-                if (grid[r][c] != '0'){
-                    bfs(r, c, r_len, c_len, grid);
-                    ni++;
+        int r = grid.length;
+        int c = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                if (grid[i][j] == '1'){
+                    bfs(grid, i, j, r, c);
+                    res++;
                 }
             }
         }
-        return ni;
+        return res;
     }
-
 }
