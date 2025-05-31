08f40cb539c86e2be5a6e421f4a445150848e03c
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-05-31 17:04:56 +0800

    Time: 247 ms (35.46%), Space: 122.4 MB (5.24%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,8 +1,7 @@
 class WordDictionary {
-    class PrefixTree {
+    class PrefixTree{
         HashMap<Character, PrefixTree> hs = new HashMap<>();
         boolean isEnd = false;
-
     }
     PrefixTree pf;
     public WordDictionary() {
@@ -11,35 +10,32 @@ class WordDictionary {
     
     public void addWord(String word) {
         PrefixTree cur = pf;
-        for (char c: word.toCharArray()){
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
             if (!cur.hs.containsKey(c)) cur.hs.put(c, new PrefixTree());
+
             cur = cur.hs.get(c);
         }
         cur.isEnd = true;
     }
-    
-    public boolean dfs(String word, int i, PrefixTree cur){
-        if (i == word.length()) return cur.isEnd;
-        char c = word.charAt(i);
+    public boolean dfs(PrefixTree cur, String word, int i){
+        if (i == word.length()) return cur.isEnd; 
+        
         if (word.charAt(i) == '.'){
-            int r = 0;
-            for (PrefixTree nc: cur.hs.values()){
-                r+= dfs(word, i+1, nc) ? 1 : 0;
-                if (r > 0) break;
-            }
-            return (r == 0) ? false: true;
+            for (PrefixTree c: cur.hs.values())
+                if (dfs(c, word, i+1)) return true;
+            return false;
+        } else if (cur.hs.containsKey(word.charAt(i))){
+            return dfs(cur.hs.get(word.charAt(i)), word, i+1);
+        }
 
-        }
-        else if (cur.hs.containsKey(c)){
-            return dfs(word, i+1, cur.hs.get(c));
-        }
         return false;
+        
     }
-
+    
     public boolean search(String word) {
         PrefixTree cur = pf;
-        
-        return dfs(word, 0, cur);
+        return dfs(cur, word, 0);
     }
 }
 
