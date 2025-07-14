b4e9d94bc3e5468e4ffe01a1fca966df7cc85fa8
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-07-14 21:10:50 +0800

    Time: 0 ms (100%), Space: 42.9 MB (46.16%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,20 @@
 class Solution {
-    public void Dfs(List<List<Integer>> res, List<Integer> cur, int[] nums, int i){
-        if (i == nums.length){
-            res.add(new ArrayList<Integer>(cur));
+    public void dfs(List<List<Integer>> res, List<Integer> cur, int i, int[]nums){
+        if (i >= nums.length){
+            res.add(new ArrayList<>(cur));
             return;
         }
-
         cur.add(nums[i]);
-        Dfs(res, cur, nums, i+1);
+        dfs(res, cur, i+1, nums);
 
         cur.remove(cur.size() - 1);
-        Dfs(res, cur, nums, i+1);
-
+        dfs(res, cur, i+1, nums);
+        
     }
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res= new ArrayList<>();
         List<Integer> cur = new ArrayList<>();
-        Dfs(res, cur, nums, 0);
+        dfs(res, cur, 0, nums);
         return res;
     }
 }
