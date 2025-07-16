205082cd8cc958997b0d9877215883af4c275615
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-07-16 17:58:45 +0800

    Time: 2 ms (83.39%), Space: 45.8 MB (55.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,6 +3,7 @@ class Solution {
         int steps = 0;
         for (int i = 0; i < nums.length -1; i++){
             int n = nums[i];
+            if (n >= nums.length -1 ) return true;
             if (steps < n) {
                 steps = n;
             } else if (steps <= 0) return false;
