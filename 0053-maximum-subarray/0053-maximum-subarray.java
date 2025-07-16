71868cd929ae669c2142ab5c13af6fc802c0e9ac
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-07-16 13:47:01 +0800

    Time: 1 ms (99.51%), Space: 57.3 MB (31.74%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,7 +3,8 @@ class Solution {
         int sum = nums[0];
         int cur = 0;
 
-        for (int n : nums){
+        for (int i = 0; i < nums.length; i++){
+            int n = nums[i];
             cur = cur < 0 ? n : cur + n;
             sum = Math.max(cur, sum); 
         }
