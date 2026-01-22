b053eae92db87e8a17f80039d5b47e8b04c3ef60
Rainn <94624435+xinzhao2627@users.noreply.github.com>
2026-01-22 14:01:27 +0800

    Time: 1 ms (97.58%), Space: 47.1 MB (64.66%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        ArrayList<int[]> res = new ArrayList<>();
-
-        for (int i = 0; i < intervals.length; i++){
-            if (newInterval[1] < intervals[i][0]){
+        List<int[]> res = new ArrayList<>();
+        for (int j = 0; j < intervals.length; j++){
+            int[] i = intervals[j];
+            // if new interval is before this existing interval
+            if (newInterval[1] < i[0]){
                 res.add(newInterval);
-                for (int j = i; j < intervals.length; j++){
-                    res.add(intervals[j]);
+                for (int c = j; c < intervals.length; c++){
+                    res.add(intervals[c]);
                 }
                 return res.toArray(new int[res.size()][]);
             }
-            else if (intervals[i][1] < newInterval[0]){
-                res.add(intervals[i]);
-            } else {
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-
+            // if new interval is after this exisitng interval
+            else if (newInterval[0] > i[1]){
+                res.add(i);
             }
-        } 
+            // if atleast there is an integer overlap
+            else {
+                newInterval[0] = Math.min(i[0], newInterval[0]);
+                newInterval[1] = Math.max(i[1], newInterval[1]);
+            }
+        }
+        
         res.add(newInterval);
+        
         return res.toArray(new int[res.size()][]);
     }
 }
